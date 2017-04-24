2775d49f5fe9a25a8b2fb6dd4feaab093e107771
shearwavesplitter <s.mroczek@outlook.com>
2017-04-24 19:03:16 +1200

    Update solve_wrap1.R

--- R/solve_wrap1.R
+++ R/solve_wrap1.R
@@ -1,27 +1,27 @@
 #' @title Mass balance and charge solver
 #' @description Mass balance and charge balance solver for chemical equilibria. 
 #' @param Tc Temperature (degrees C)
-#' @param Nat Sodium concentration (mol/kg)
-#' @param Kt Potassium (mol/kg)
-#' @param Clt Chlorine (mol/kg)
-#' @param SO4t Sulphate (mol/kg)
-#' @param Cat Calcium (mol/kg)
-#' @param Mgt Magnesium (mol/kg)
+#' @param Nat Sodium concentration (mol/kg); total 
+#' @param Kt Potassium (mol/kg); total
+#' @param Clt Chlorine (mol/kg); total
+#' @param SO4t Sulphate (mol/kg); total
+#' @param Cat Calcium (mol/kg); total
+#' @param Mgt Magnesium (mol/kg); total
 #' @param start Initial guess for the calculated equalibrium concentration of the basis species
 #' @param maxitr Maximum number of iteration
-#' @param exprod A vector of the names of the additional product species which react to form the basis species
-#' @param exconstit  A vector of the chemical symbols of the additional product species in terms of the basis species
-#' @param exnumz A vector of the number of basis consituents for each of the additional product species given by the equilibrium equation
-#' @param excharges A vector of the total charage of the additional product species
-#' @param exK A vector of the log K values of the additional product species
-#' @param exa A vector of the ion size paramters for the additional species
+#' @param exprod A vector of the names of the complexes which dissociate to form the basis species
+#' @param exconstit  A vector of the chemical symbols names of the complexes in terms of the basis species
+#' @param exnumz A vector of the stiochiometery given by the equilibrium reaction for each of the complexes
+#' @param excharges A vector of the charage of the complex species
+#' @param exK A vector of the log K of the dissociation constants
+#' @param exa A vector of the ion size paramters for the complexes
 #' @details A wrapper for the chemsolve_generic function that allow easy addition of product species. If you want to add additional reactant species (i.e. basis species) then the chemsolve_generic function must be used
-#' @return A list containing the concentrations, gamma values, and pH at equilibrium
+#' @return A list containing the concentrations, activity coefficients, and pH at equilibrium
 #' @export
 #' @examples
-#' ## Add H2SO4 as an extra product species
+#' ## Add H2SO4 as an additional complex given the existing list of basis species
 #'
-#' chemsolve(exprod="H2SO4",exconstit="H","H","SO4",exnumz=3,excharges=0,exa=0,exa=-6)
+#' chemsolve(exprod="H2SO4",exconstit="H","H","SO4",exnumz=3,excharges=0,exa=0,exK=-6)
 #' 
 chemsolve <- function(Tc=300,Nat=0.2,Kt=0.2,Clt=0.4,SO4t=0.2,Cat=0.1,Mgt=0.1,start=c(0.00001,0.00001,0.15,0.15,0.15,0.104756881,0.05,0.05),maxitr=100,exprod=NULL,exconstit=NULL,exnumz=NULL,excharges=NULL,exa=NULL,exK=NULL) {
 
